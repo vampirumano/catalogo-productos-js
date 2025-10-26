59c593c2749f6806a15114f856e79a7cdcd2e6ae
Carlos Tapia <vampirumano@gmail.com>
2025-10-25 20:48:54 -0500

    Primer commit: versión inicial del catálogo con filtrado

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+import './miestilo.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import './moduls/catalogo.js'
+import './moduls/eventos.js'
+
+document.querySelector('#app').innerHTML = `
+    <div>
+        
+    </div>
+`
+
---
+++ src/moduls/datos/datos.js
@@ -0,0 +1,84 @@
+const productos = [
+    { 
+        id: 1, 
+        nombre: "Producto A", 
+        descripcion: "Descripción detallada del Producto A, resaltando sus características y beneficios.", 
+        precio: 10.99, 
+        categoria: "gafas-polarizadas", 
+        imagen: ["https://res.cloudinary.com/dvdz1osso/image/upload/w_1024,c_limit,q_auto,f_auto/CD4951_01_cpxyna", "ruta/a/imagenA2.jpg", "ruta/a/imagenA3.jpg"] 
+    }, 
+    { 
+        id: 2, 
+        nombre: "Producto B", 
+        descripcion: "Descripción detallada del Producto B, con énfasis en su calidad y uso.", 
+        precio: 15.5, 
+        categoria: "gafas-polarizadas", 
+        imagen: ["https://res.cloudinary.com/dvdz1osso/image/upload/Lacoste_L1003P_blanca_b1dmnx.jpg", "ruta/a/imagenB2.jpg", "ruta/a/imagenB3.jpg"] 
+    }, 
+    { 
+        id: 3, 
+        nombre: "Producto C", 
+        descripcion: "Descripción detallada del Producto C, ideal para diferentes necesidades.", 
+        precio: 8.75, 
+        categoria: "gafas-infantiles", 
+        imagen: ["https://res.cloudinary.com/dvdz1osso/image/upload/v1760453545/Gafas_de_sol_polarizadas_Lacoste_L1003P_11_bl8i81.jpg", "ruta/a/imagenC2.jpg", "ruta/a/imagenC3.jpg"] 
+    }, 
+    { 
+        id: 4, 
+        nombre: "Producto D", 
+        descripcion: "Descripción detallada del Producto D, con características únicas.", 
+        precio: 12.0, 
+        categoria: "accesorios", 
+        imagen: ["https://res.cloudinary.com/dvdz1osso/image/upload/v1760453542/Gafas_de_sol_polarizadas_Lacoste_L1003P_06_id6egn.jpg", "ruta/a/imagenD2.jpg", "ruta/a/imagenD3.jpg"] 
+    }, 
+    { 
+        id: 5, 
+        nombre: "Producto E", 
+        descripcion: "Descripción detallada del Producto E, ideal para uso diario.", 
+        precio: 9.99, 
+        categoria: "Categoría 2", 
+        imagen: ["https://res.cloudinary.com/dvdz1osso/image/upload/v1760453542/Gafas_de_sol_polarizadas_Lacoste_L1003P_03_zmtwv9.jpg", "ruta/a/imagenE2.jpg", "ruta/a/imagenE3.jpg"]
+    }, 
+    { 
+        id: 6, 
+        nombre: "Producto F", 
+        descripcion: "Descripción detallada del Producto F, con diseño moderno.", 
+        precio: 20.0, 
+        categoria: "gafas-infantiles", 
+        imagen: ["https://res.cloudinary.com/dvdz1osso/image/upload/v1760453541/Gafas_de_sol_polarizadas_Lacoste_L1003P_02_jvzqyu.jpg", "ruta/a/imagenF2.jpg", "ruta/a/imagenF3.jpg"] 
+    }, 
+    { 
+        id: 7, 
+        nombre: "Producto G", 
+        descripcion: "Descripción detallada del Producto G, resistente y duradero.", 
+        precio: 18.5, 
+        categoria: "accesorios", 
+        imagen: ["https://res.cloudinary.com/dvdz1osso/image/upload/v1760453541/Gafas_de_sol_polarizadas_Lacoste_L1003P_01_xpnvck.jpg", "ruta/a/imagenG2.jpg", "ruta/a/imagenG3.jpg"] 
+    }, 
+    { 
+        id: 8, 
+        nombre: "Producto H", 
+        descripcion: "Descripción detallada del Producto H, fácil de usar.", 
+        precio: 7.25, 
+        categoria: "Categoría 2", 
+        imagen: ["https://res.cloudinary.com/dvdz1osso/image/upload/v1760478002/CD4951_06_zeten5.jpg", "ruta/a/imagenH2.jpg", "ruta/a/imagenH3.jpg"] 
+    }, 
+    { 
+        id: 9, 
+        nombre: "Producto I", 
+        descripcion: "Descripción detallada del Producto I, compacto y eficiente.", 
+        precio: 14.75, 
+        categoria: "gafas-infantiles", 
+        imagen: ["https://res.cloudinary.com/dvdz1osso/image/upload/v1760478000/CD4951_04_gezf0x.jpg", "ruta/a/imagenI2.jpg", "ruta/a/imagenI3.jpg"] 
+    }, 
+    { 
+        id: 10, 
+        nombre: "Producto J", 
+        descripcion: "Descripción detallada del Producto J, con excelente relación calidad-precio.", 
+        precio: 11.5, 
+        categoria: "accesorios", 
+        imagen: ["https://res.cloudinary.com/dvdz1osso/image/upload/v1760584686/RB4171T_02_xf8wdw.jpg", "ruta/a/imagenJ2.jpg", "ruta/a/imagenJ3.jpg"] 
+    }
+];
+
+export default productos;
